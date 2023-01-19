44d7b0a4434f2152cf6668b625533d6ca104f19d
acoustic_jeet <js.jitendra11@gmail.com>
2023-01-19 23:23:34 +0530

    Create String_next()andnextLine().java

---
+++ String_next()andnextLine().java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.nextLine();
+        System.out.println(str);
+        str = sc.next();
+        System.out.println(str);
+        
+	}
+}
