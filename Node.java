c197056e5ee3ceb522690d34e759bf7bca6f95b3
acoustic_jeet <js.jitendra11@gmail.com>
2023-03-23 16:40:35 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,11 @@
+package linkedlist2;
+
+public class Node {
+	int data;
+	Node next;
+	//Constructor to initialise values in the beginnning itself.
+		Node(int data) {
+		this.data=data;
+		//next = null; This thing not needed since by default hi null hota h
+		}
+}
